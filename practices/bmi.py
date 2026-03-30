484d9f9597dd41aa066edd0605ab349b27b030ba
Jack Yeh <jao.chin@gmail.com>
2026-03-30 17:54:33 +0800

    Miner updates.

--- practices/bmi.py
+++ practices/bmi.py
@@ -1,4 +1,5 @@
-height=eval(input("請輸入身高 (m)："))
-weight=eval(input("請輸入體重 (kg)："))
-bmi=weight/(height*height)
-print(f"\n身高{height:>5.2f}m, 體重{weight:>5.2f}kg, BMI = {bmi:>5.2f}.", end="\n\n")
+height = eval(input("請輸入身高 (cm)："))
+weight = eval(input("請輸入體重 (kg)："))
+heightM = height / 100
+bmi = weight / (heightM * heightM)
+print(f"\n身高{height:>5.2f}cm, 體重{weight:>5.2f}kg, BMI = {bmi:>5.2f}.", end="\n\n")
--- practices/test.py
+++ practices/test.py
@@ -1,3 +1,5 @@
+SKIP_ONE_LINE = "\n\n"
+
 # Basic mathmatical calculation.
 PI = 3.14159
 r = 5
@@ -7,12 +9,27 @@ print(
     "r = %d\tarea of the circle = %f\ndimension = %d\tlength = %f"
     % (r, area, 2 * r, 2 * PI * r)
 )
-print("PI = %s\tr = %s\tarea = %s" % (type(PI), type(r), type(area)), end="\n\n")
+print("PI = %s\tr = %s\tarea = %s" % (type(PI), type(r), type(area)), end=SKIP_ONE_LINE)
 
 # # Input and output.
-inp = input("Input a value : ")
-inpVal = eval(inp)
-print(
-    "Your input is %s, and type of your input is %s.\nDigit : %f"
-    % (inp, type(inpVal), inpVal)
-)
+# inp = input("Input a value : ")
+# inpVal = eval(inp)
+# print(
+#     "Your input is %s, and type of your input is %s.\nDigit : %f"
+#     % (inp, type(inpVal), inpVal)
+# )
+
+# Reverse a string.
+string = "Hello world!"
+print(string[::-1], end=SKIP_ONE_LINE)
+
+# String mainuplation.
+parts = string.split()
+print(f"type of parts = {type(parts)}, parts = {parts}")
+
+join = "_".join(parts)
+print(f"Joined string = {join}", end=SKIP_ONE_LINE)
+
+org = "Hello"
+to = "Hi,"
+print(f"Replace '{org}' by '{to}' -> {string.replace(org, to)}", end=SKIP_ONE_LINE)
